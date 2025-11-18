d9ce9c025d54377c23494fa738e7353eac111d66
Liiigia <ligiaalencar13@gmail.com>
2025-11-18 20:49:39 +0300

    iniciar arquivo teste.py

---
+++ exemplo1.py
@@ -0,0 +1,41 @@
+# n1 = 'Maria'
+# print(f'Meu nome é{n1}')
+# nome_usuario = input('informe o nome:')
+# idade_usuario = input('informe a idade:')
+# print(f'O nome é{nome_usuario} e sua idade {idade_usuario}')
+# nome_cidade = input('informe a cidade:')
+# nome_bairro = input('informe o bairro:')
+# print(f'o nome da cidade é {nome_cidade} e o bairro é {nome_bairro}')
+# num1 = float(input('informe o numero:'))
+# num2 = float(input('informe o segundo numero:'))
+# resultado = num1 * num2
+# print(resultado)
+# ano_atual = float(input('informe o ano atual:'))
+# idade_usuario = float(input('informe a sua idade:'))
+# resultado = ano_atual - idade_usuario
+# print(resultado)
+# Montante_total = float(input('total de dinheiro que tenho:'))
+# valor_ingresso = float(input('preço do ingresso:'))
+# resultado = Montante_total/valor_ingresso
+# print(resultado)
+# preco = float(input(f'informe o preco:'))
+# qtd = int(input('informe a quantidade'))
+# total = preco * qtd
+# desconto = total * 10/100
+# valor_final =total - desconto
+# print(f'valor total {total}\nDesconto de 10% {desconto}\nValor a pagar: {valor_final}')
+# salario_inicial = float(input('informar o salario inicial: '))
+# acrescimo_salario = float(input('informar o o percentual de aumento: '))
+# acrescimo = salario_inicial * 0.15
+# resultado = salario_inicial + acrescimo_salario
+# print(f'salario final pós acrescimo: {resultado}'
+# precoanterior_camisa = float(input('informar o preço da camisa na semana passa:'))
+# desconto = float(input('informar o percentual de desconto:'))
+# valor_desconto = precoanterior_camisa*desconto
+# resultado = precoanterior_camisa - desconto
+# print(resultado)
+# valor_vendas = float(input('informar o valor vendido:'))
+# metas_vendas = float(input('informar a meta de vendas:'))
+# resultado = valor_vendas/metas_vendas
+# print(resultado)
+                     
